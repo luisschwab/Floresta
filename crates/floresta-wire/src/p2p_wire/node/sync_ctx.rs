b108023605ac6c6a083ba2aba60225d6fecbdd74
Davidson Souza <me@dlsouza.lol>
2026-03-23 11:30:48 -0300

    chore: increase timeout to two minutes

--- crates/floresta-wire/src/p2p_wire/node/sync_ctx.rs
+++ crates/floresta-wire/src/p2p_wire/node/sync_ctx.rs
@@ -48,7 +48,7 @@ impl NodeContext for SyncNode {
         ServiceFlags::NETWORK | ServiceFlags::WITNESS | service_flags::UTREEXO_ARCHIVE.into()
     }
 
-    const REQUEST_TIMEOUT: u64 = 60; // 1 minute
+    const REQUEST_TIMEOUT: u64 = 60 * 2; // 2 minutes
     const MAX_INFLIGHT_REQUESTS: usize = 100; // double the default
 
     // A more conservative value than the default of 1 second, since we'll have many peer messages
